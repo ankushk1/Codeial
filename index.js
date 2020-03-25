810f158de995fffde209cd9237dd7c739f17ed10
ankushk1 <40850518+ankushk1@users.noreply.github.com>
2020-03-25 15:02:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8002;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
+
+//chat server used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server listening on 5000");
+
+
+//saas to css
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+//assets
+app.use(express.static('./assets'));
+
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uplo ads'));
+
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'something',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore({
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+
+        },
+        function (err) {
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
+
+// use express router
+app.use('/', require('./routes'));
+
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
